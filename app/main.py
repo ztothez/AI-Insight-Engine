4bfc15f3dcf111609b0664553ad90a66e6cf5d2b
ztothez <ztothez@users.noreply.github.com>
2026-04-14 00:47:41 +0300

    day-1-boot-camp-done

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import os
 from fastapi import FastAPI
+from fastapi_health import health
 from app.routes.chat import router as chat_router
 
 app = FastAPI()
@@ -9,3 +10,7 @@ favicon_path = "favicon.ico"
 def favicon():
     return {"message": "This is the favicon endpoint."}
 app.include_router(chat_router)
+
+@app.get("/health")
+def health_check():
+    return {"status": "200 OK"}
